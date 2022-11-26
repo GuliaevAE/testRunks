45d5b3821c594134a8cf5faa1111daf23759c721
Anton <antybumer@gmail.com>
2022-11-27 01:02:43 +0300

    add vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - pr',
-    title: 'Exchange Rates',
+    title: 'exchange-rates',
     htmlAttrs: {
       lang: 'en'
     },
