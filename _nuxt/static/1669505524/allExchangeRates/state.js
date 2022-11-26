a9375310fa57842f39bf4d3ae99ee30d59540117
Anton <antybumer@gmail.com>
2022-11-27 02:32:09 +0300

    Updates

---
+++ _nuxt/static/1669505524/allExchangeRates/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FtestRunks\u002F_nuxt\u002Fstatic\u002F1669505524",layout:"default",error:a,serverRendered:true,routePath:"\u002FallExchangeRates",config:{_app:{basePath:"\u002FtestRunks\u002F",assetsPath:"\u002FtestRunks\u002F_nuxt\u002F",cdnURL:a}}}}(null));
